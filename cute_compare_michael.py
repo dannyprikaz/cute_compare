829b2924f8bbc9d0ff7b5d7f429faae2046761e3
aurora-novis <michael.albada@gmail.com>
2016-07-26 18:54:37 -0700

    Add styled first page, but does not successfully return values

--- cute_compare_michael.py
+++ cute_compare_michael.py
@@ -7,34 +7,14 @@ from random import randint
 @app.route('/')
 def index():
     return render_template('index.html')
-    #
-    # '''
-    # <h1> Cute Compare </h1>
-    # <form action="/predict" method='POST' >
-    # <input type="text" name="object1" />
-    # <input type="submit" />
-    # </form>
-    # <form action="/predict" method='POST' >
-    # <input type="text" name="object2" />
-    # <input type="submit" />
-    # </form>
-    # '''
 
-def random_mammals()
+def random_mammals():
     with open('mammals.txt') as f:
         mammals = f.readlines()
         mammals = [[m.strip('\n')] for m in mammals]
         rand_mammal_1 = mammals[randint(0,len(mammals))]
         rand_mammal_2 = mammals[randint(0,len(mammals))]
     return rand_mammal_1, rand_mammal_2
-
-# compare page
-@app.route('/predict', methods=['POST'])
-def predict():
-    article = str(request.form['user_input'])
-    article_tfidf = vectorizer.fit_transform(article)
-    prediction = model.predict(article_tfidf)
-    pass
 
 # loads the pickled model and vectorizer
 def load_model():
